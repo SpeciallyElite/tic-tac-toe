69606078adf11f262527433e700d18989e92df1d
SpeciallyElite <bhardwazaarya@gmail.com>
2026-02-21 14:44:18 +0000

    The beggining is the hardest and i have been stuck doing this the whole day i really thought it was going to be easy but when you are learning concepts while putting them concepts into an actual project you realize this coding stuff is not easy bruh

--- script.js
+++ script.js
@@ -1,20 +1,47 @@
 humanityInput = null;
 machineInput = null;
+const positions = [];
 
 function gameBoard(){
-    const positions = [p1,p2,p3,p4,p5,p6,p7,p8,p9];
-    const newPositions = [];
-    newInput = input;
-    if (newPositions.includes(newInput)){
-        return console.log("Wrong Move");
+    if (positions[input] === null){
+        positions[input] = currentPlayer;
     }
     else{
-        newPositions.push(newInput);
+        return console.log("Wrong Move cuz");
     }
-    return logic(newInput);
+
+    // newInput = input
+    // if (positions.includes(newInput)){
+    //     return console.log("Wrong Move");
+    // }
+    // else{
+    //     positions.push(newInput);
+    // }
+    // return logic(newInput);
 }
 
 function logic(){
     gameBoard(input);
-    
+    winningConditions =
+    [[0, 1, 2], [3, 4, 5], [6, 7, 8],     // Rows
+    [0, 3, 6], [1, 4, 7], [2, 5, 8],     // Columns
+    [0, 4, 8], [2, 4, 6]]               //Dia-ag-nolassssss mf [my friend :) ]
+
+    for (i=0; i<winningConditions.length; i++){
+        // const combination = winningConditions[i];
+        // const a = combination[0];
+        // const b = combination[1];
+        // const c = combination[2]; Basically tha'ts what's below in one line
+
+        const [a,b,c] = winningConditions[i];
+        
+        const pA = positions[a];
+        const pB = positions[b];
+        const pC = positions[c];
+
+        if (pA!=="null" && pA===pB===pC){
+            return pA;
+        }
+    }
+
 }
